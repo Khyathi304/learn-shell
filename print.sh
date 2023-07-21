d788b78e3bd46c8187b2bd2b47735cae61defb93
Khyathi304 <khyathikamireddy04@gmail.com>
2023-07-21 17:19:23 +0100

    Print Colors

--- print.sh
+++ print.sh
@@ -1,1 +1,18 @@
 echo HelloWorld
+
+# Red       31
+# Green     32
+# Yellow    33
+# Blue      34
+# Magenta   35
+# Cyan      36
+
+echo -e "\e[CODEmMESSAGE\e[0m"
+
+echo -e "\e[31mHI\e[0m"
+echo -e "\e[32mHI\e[0m"
+echo -e "\e[33mHI\e[0m"
+echo -e "\e[34mHI\e[0m"
+echo -e "\e[35mHI\e[0m"
+echo -e "\e[36mHI\e[0m"
+
