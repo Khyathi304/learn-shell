b7ae38fee3a2957b109619a9bd2fb3108ba05f28
Khyathi304 <khyathikamireddy04@gmail.com>
2023-07-25 14:40:55 +0100

    Frontend.

--- loops.sh
+++ loops.sh
@@ -7,4 +7,10 @@ a=10
 while [ $a -gt 0 ]; do
   echo Hello
   a=$(($a-1))
+  #break #Come out of the loop
   done
+
+#based on inputs
+  for comp in khyathi anand dhanya; do
+    echo naming $comp
+    done
