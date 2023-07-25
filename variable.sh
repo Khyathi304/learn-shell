51d7778536e042be7003eb45adf19520179b40d3
Khyathi304 <khyathikamireddy04@gmail.com>
2023-07-25 14:04:21 +0100

    Frontend.

--- variable.sh
+++ variable.sh
@@ -5,8 +5,8 @@ echo a is $a or ${a}
 #Special Variables
 ##Comannd Substitution:
 DATE=${date}
-echo Todays date is $DATE.
+echo Todays date is $DATE
 
 ##Arithmetic Substitution
-ADD=$((2+2))
-echo Adiition of ((2+2)) is $ADD
+ADD=$(( 2+2 ))
+echo Adiition of 2+2 is $ADD
