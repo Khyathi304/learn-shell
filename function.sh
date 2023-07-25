5be3c7bf7d3e30407bad55a9439b50c46dfa96e0
Khyathi304 <khyathikamireddy04@gmail.com>
2023-07-25 14:17:06 +0100

    Frontend.

--- function.sh
+++ function.sh
@@ -6,3 +6,15 @@ greeting(){
 }
 greeting
 echo function Exit Status - $?
+
+#you can declare var in main program, you can access that in the main function and vice-versa
+#function have its own special variables
+
+input() {
+  echo First Argument $1
+  echo Second Argument $2
+  echo All Arguments $*
+  echo No. of Arguments $#
+}
+
+input Khyathi Anand Dhanya
